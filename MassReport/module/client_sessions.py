40f5ef9013f82b5c2510b55670b0787b04437ed6
ItsSanatani <sachinxmeena2000@gmail.com>
2025-03-18 16:15:37 +0530

    MassReport !!

---
+++ MassReport/module/client_sessions.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, SESSION_STRINGS
+
+clients = []
+
+for idx, session_str in enumerate(SESSION_STRINGS, start=1):
+    client = Client(
+        f"session_{idx}",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=session_str
+    )
+    clients.append(client)
