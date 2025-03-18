eb8cb4a7d8cf38f5f4d992ee9a3b612258d45a28
ItsSanatani <sachinxmeena2000@gmail.com>
2025-03-18 16:07:41 +0530

    MassReport !!

---
+++ MassReport/__main__.py
@@ -0,0 +1,7 @@
+from MassReport import app
+from MassReport.modules import report
+from MassReport.database import database
+
+if __name__ == "__main__":
+    print("Mass Report Bot Started!")
+    app.run()
