e86b73b53ab5c210695c1351c51e273a31d313bd
ItsSanatani <sachinxmeena2000@gmail.com>
2025-03-18 17:25:54 +0530

    MassReport !!

--- MassReport/database/database.py
+++ MassReport/database/database.py
@@ -1,10 +1,21 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 user_data = {}
 
 def get_user_data(user_id):
-    return user_data.get(user_id, {})
+    data = user_data.get(user_id, {})
+    logger.info(f"Fetching data for user_id: {user_id} | Data: {data}")
+    return data
 
 def set_user_data(user_id, data):
     user_data[user_id] = data
+    logger.info(f"Setting data for user_id: {user_id} | Data: {data}")
 
 def clear_user_data(user_id):
-    user_data.pop(user_id, None)
+    removed = user_data.pop(user_id, None)
+    if removed:
+        logger.info(f"Cleared data for user_id: {user_id}")
+    else:
+        logger.info(f"No data found to clear for user_id: {user_id}")
